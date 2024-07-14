b872d5ff5e1c02376d069031e5d011ca2ee8be79
Kieran David Evans <keyz182@gmail.com>
2024-07-14 10:59:13 +0100

    Cookie cutter initial commit

---
+++ 1.5/Source/AvoidDarkness/AvoidDarknessMod.cs
@@ -0,0 +1,33 @@
+﻿using Verse;
+using UnityEngine;
+using HarmonyLib;
+
+namespace AvoidDarkness;
+
+public class AvoidDarknessMod : Mod
+{
+    public static Settings settings;
+
+    public AvoidDarknessMod(ModContentPack content) : base(content)
+    {
+
+        // initialize settings
+        settings = GetSettings<Settings>();
+#if DEBUG
+        Harmony.DEBUG = true;
+#endif
+        Harmony harmony = new Harmony("keyz182.rimworld.AvoidDarkness.main");	
+        harmony.PatchAll();
+    }
+
+    public override void DoSettingsWindowContents(Rect inRect)
+    {
+        base.DoSettingsWindowContents(inRect);
+        settings.DoWindowContents(inRect);
+    }
+
+    public override string SettingsCategory()
+    {
+        return "AvoidDarkness_SettingsCategory".Translate();
+    }
+}
